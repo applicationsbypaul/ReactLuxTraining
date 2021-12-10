2f7ebdd612090cbd739a70e7061b4c8372e8ec9b
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2021-12-10 11:16:31 -0600

    Added routing and error Page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Header() {
+  const activeStyle = { color: "orange" };
+  const activeStyle2 = { color: "green" };
   return (
     <nav>
-      <a href="/">Home</a> | <a href="/courses">Courses</a> |{" "}
-      <a href="/about">About</a>
+      <NavLink activeStyle={activeStyle} exact to="/"> 
+        Home
+      </NavLink>
+      |{" | "}
+      <NavLink activeStyle={activeStyle2} to="/courses">
+        Courses
+      </NavLink>
+      {" | "}
+      <NavLink activeStyle={activeStyle} to="/about">
+        About
+      </NavLink>
     </nav>
   );
 }
