72c4715d94c84e2647680058084815161e148da2
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2021-12-23 13:50:22 -0600

    Updated a slug usage for ManageCoursePage

---
+++ src/components/ManageCoursePage.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const ManageCoursePage = props => {
+    return(
+        <>
+            <h2>Manage Course</h2>
+            {props.match.params.slug}
+        </>
+    )
+}
+
+export default ManageCoursePage;
