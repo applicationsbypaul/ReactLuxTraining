efd157ac1486c84f30dc46177319b2a54ec71c88
PaulFord <applicationsbypaul@gmail.com>
2022-04-16 18:34:33 -0500

    confrimed a new form

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,26 +1,65 @@
-import React, { useState }  from 'react';
-import CourseForm from './CourseForm';
+import React, { useState, useEffect } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify"
 
-const ManageCoursePage = props => {
-    const [ course, setCourse ] = useState({
+const ManageCoursePage = (props) => {
+    const [errors, setErrors] = useState({});
+    const [course, setCourse] = useState({
         id: null,
         slug: "",
         title: "",
         authorId: null,
-        category: ""
-    });
+        category: "",
+  });
 
-    function handleChange(event) {
-        const updatedCourse = {...course, [event.target.name]: event.target.value};
-        setCourse(updatedCourse);
-    }
-    return(
-        <>
-            <h2>Manage Course</h2>
-            <CourseForm course = {course} onChange={handleChange}></CourseForm>
-            {props.match.params.slug}
-        </>
-    )
-}
+  useEffect(() => {
+      const slug = props.match.params.slug;
+      if (slug) {
+          courseApi.getCourseBySlug(slug).then(_course => setCourse(_course))
+      }
+  }, [props.match.params.slug]);
+
+  function handleChange(event) {
+    const updatedCourse = {
+      ...course,
+      [event.target.name]: event.target.value,
+    };
+    setCourse(updatedCourse);
+  }
+
+  function formIsValid(){
+      const _errors = {};
+
+      if (!course.title) _errors.title = "Title is required";
+      if (!course.authorId) _errors.authorId = "Author ID is required";
+      if (!course.category) _errors.category = "Category is required";
+
+      setErrors(_errors)
+
+      return Object.keys(_errors).length === 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!formIsValid()) return;
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved.");
+    });
+  }
+
+  return (
+    <>
+      <h2>Manage Course</h2>
+      <CourseForm
+        errors={errors}
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      ></CourseForm>
+    </>
+  );
+};
 
 export default ManageCoursePage;
