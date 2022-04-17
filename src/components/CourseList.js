a29fbe4c12db657763f47c4c22540fec0348e754
PaulFord <applicationsbypaul@gmail.com>
2022-04-17 01:35:26 -0500

    added a delete function to the app

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function CourseList(props) {
   return (
@@ -10,6 +11,7 @@ function CourseList(props) {
           <th>Title</th>
           <th>Author ID</th>
           <th>Category</th>
+          <th>Delete</th>
         </tr>
       </thead>
       <tbody>
@@ -21,6 +23,15 @@ function CourseList(props) {
               </td>
               <td>{course.authorId}</td>
               <td>{course.category}</td>
+              <td>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => props.deleteCourse(course.id).then(() =>
+                  toast.success("Course Deleted Successfully"))}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           );
         })}
@@ -30,6 +41,7 @@ function CourseList(props) {
 }
 
 CourseList.propTypes = {
+  deleteCourse: PropTypes.func.isRequired,
   courses: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
